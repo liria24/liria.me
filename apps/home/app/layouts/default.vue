ef32e1a83d2f4821b2c446806533f604c8f8ee68
Liry24 <47878070+Liry24@users.noreply.github.com>
2025-08-01 15:48:41 +0900

    remove unused locale handling and clean up layout components

--- apps/home/app/layouts/default.vue
+++ apps/home/app/layouts/default.vue
@@ -1,39 +1,23 @@
 <script lang="ts" setup>
-const head = useLocaleHead()
 const title = 'Liria'
 </script>
 
 <template>
-    <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
+    <Html>
         <Head>
             <Title>{{ title }}</Title>
-            <template v-for="link in head.link" :key="link.hid">
-                <Link
-                    :id="link.hid"
-                    :rel="link.rel"
-                    :href="link.href"
-                    :hreflang="link.hreflang"
-                />
-            </template>
-            <template v-for="meta in head.meta" :key="meta.hid">
-                <Meta
-                    :id="meta.hid"
-                    :property="meta.property"
-                    :content="meta.content"
-                />
-            </template>
         </Head>
         <Body>
             <UContainer class="flex min-h-[100dvh] w-full flex-col gap-16">
                 <header class="mt-6 flex w-full items-center justify-between">
-                    <NuxtLinkLocale to="/" class="flex items-center gap-2.5">
+                    <NuxtLink to="/" class="flex items-center gap-2.5">
                         <NuxtImg
                             src="/logo/liria.svg"
                             alt="Liria"
                             :width="32"
                             class="size-8"
                         />
-                    </NuxtLinkLocale>
+                    </NuxtLink>
 
                     <div class="flex items-center gap-2">
                         <UButton
