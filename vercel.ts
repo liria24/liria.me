2f5a48abab6ee44f292afff8733a4317de4fc8bf
Liry24 <liry241999@gmail.com>
2025-12-22 15:42:42 +0900

    feat: Add permanent redirect for `/liry24` to `liry24.com` and update bun dependencies.

--- vercel.ts
+++ vercel.ts
@@ -1,6 +1,8 @@
-import type { VercelConfig } from '@vercel/config/v1'
+import { routes, type VercelConfig } from '@vercel/config/v1'
 
 export const config: VercelConfig = {
     cleanUrls: true,
     trailingSlash: false,
+
+    redirects: [routes.redirect('/liry24', 'https://liry24.com', { permanent: true })],
 }
